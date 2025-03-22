acf5b7bb6d514e9a3ef3adc092ff99b485212dd3
Coder <89525795+DebugDyno@users.noreply.github.com>
2025-03-22 11:18:23 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,7 @@
-<template>
+
   <main className="flex flex-col justify-center">
     {/* Display the "Hosted by Spidy" text */}
     <div className="mt-8 text-center text-gray-500">
       Hosted by Spidy
     </div>
   </main>
-</template>
