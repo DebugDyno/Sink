13f2904b4ef74f0d260470d1c884de26a34ee081
Coder <89525795+DebugDyno@users.noreply.github.com>
2025-03-22 11:42:49 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,6 @@
-
+<template>
   <div>
     <!-- Display the "Hosted by Spidy" text -->
     <p class="text-center text-gray-500 mt-8">Hosted by Spidy</p>
   </div>
-
-<script>
-export default {
-  layout: 'empty', // Use an empty layout to remove the default header/footer
-};
-</script>
-
-<style scoped>
-.text-center {
-  text-align: center;
-}
-
-.text-gray-500 {
-  color: #6b7280;
-}
-
-.mt-8 {
-  margin-top: 2rem;
-}
-</style>
+</template>
