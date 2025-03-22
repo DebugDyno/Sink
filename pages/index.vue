aed81b59e1bf6a5432b9e0db0f5367946024b563
Coder <89525795+DebugDyno@users.noreply.github.com>
2025-03-22 11:27:51 +0530

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
-<script>
-  <main className="flex flex-col justify-center">
-    <div className="mt-8 text-center text-gray-500">
+<template>
+  <main class="flex flex-col justify-center">
+     <div className="mt-8 text-center text-gray-500">
       Hosted by Spidy
     </div>
   </main>
-</script>
+</template>
